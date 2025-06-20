d210a919f7a77afa7a5e7411ccdf899ecf2f8d82
Khaled <kh99ch@gmail.com>
2025-06-20 19:46:08 +0200

    yay it works

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
-#include <raylib.h>
 #include <math.h>
+#include <raylib.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 #define WIDTH 800
 #define HEIGHT 800
@@ -14,86 +14,118 @@ typedef struct {
     Vector2 size;
     Vector2 speed;
     Color color;
-    bool on_ground;
 } Square_t;
 
-Color color_array[6] = {
-    ORANGE, YELLOW, GREEN, BLUE, PURPLE, RED
-};
+Color color_array[6] = {ORANGE, YELLOW, GREEN, BLUE, PURPLE, RED};
 int color_index = 0;
 
-void update_square(Square_t* square, Rectangle* boundry, float dt) {
-    square->speed.y += 983 * dt;
-    square->pos.y += square->speed.y * dt;
+void update_square(Square_t *square, Rectangle *boundry, Vector2 prev_boundry, Vector2 current_boundry, float dt) {
+    // Calculate boundary velocity
+    Vector2 boundry_velocity = {
+        (current_boundry.x - prev_boundry.x) / dt,
+        (current_boundry.y - prev_boundry.y) / dt
+    };
 
+    square->speed.y += 983 * dt;
+    
+    square->pos.y += square->speed.y * dt;
+    square->pos.x += square->speed.x * dt;
+
+    float min_bounce_speed = 50.0f;
+    float bounce_factor = 0.75f;
+    float floor_friction = 0.3f;
+    float wall_friction = 0.2f;
+
+    // Right wall collision
     if (square->pos.x >= (boundry->x + boundry->width) - square->size.x) {
+        color_index = color_index > 5 ? 0 : color_index;
+        square->color = color_array[color_index++];
+
+        square->speed.x = -fabs(square->speed.x) + boundry_velocity.x * wall_friction;
         square->pos.x = (boundry->x + boundry->width) - square->size.x;
     }
-    if (square->pos.x < boundry->x) square->pos.x = boundry->x;
-    if (square->pos.y > (boundry->y + boundry->height) - square->size.y) {
-        if (!square->on_ground) {
-            square->speed.y *= -0.8f;
+    // Left wall collision
+    else if (square->pos.x < boundry->x) {
+        color_index = color_index > 5 ? 0 : color_index;
+        square->color = color_array[color_index++];
+
+        square->speed.x = fabs(square->speed.x) + boundry_velocity.x * wall_friction;
+        square->pos.x = boundry->x;
+    }
+    // Floor collision
+    else if (square->pos.y > (boundry->y + boundry->height) - square->size.y) {
+        if (fabs(square->speed.y) > min_bounce_speed) {
             color_index = color_index > 5 ? 0 : color_index;
-            square->color = color_array[color_index];
-            square->on_ground = true;
-            color_index++;
+            square->color = color_array[color_index++];
+        }
+
+        square->speed.y = -fabs(square->speed.y) * bounce_factor + boundry_velocity.y;
+        square->speed.x = square->speed.x * floor_friction + boundry_velocity.x;
+
+        if (fabs(square->speed.y) < min_bounce_speed) {
+            square->speed.y = 0;
         }
         square->pos.y = (boundry->y + boundry->height) - square->size.y;
-    } else {
-        square->on_ground = false;
     }
-    if (square->pos.y < boundry->y) square->pos.y = boundry->y;
+    // Ceiling collision
+    else if (square->pos.y < boundry->y) {
+        color_index = color_index > 5 ? 0 : color_index;
+        square->color = color_array[color_index++];
 
+        square->speed.y = fabs(square->speed.y) * bounce_factor + boundry_velocity.y * wall_friction;
+        square->pos.y = boundry->y;
+    }
 }
 
 int main() {
-	InitWindow(WIDTH, HEIGHT, "Shake!");
+    InitWindow(WIDTH, HEIGHT, "Shake!");
 
     Square_t square = {
         .size = {.x = 100, .y = 100},
         .speed = {.x = 0, .y = 10},
         .color = RED,
-        .on_ground = false
     };
 
     // boundry size is the same as screen
     // so x and y are half that
-    Rectangle boundry = {
-        .x = MONITOR_WIDTH/2 - 200,
-        .y = MONITOR_HEIGHT/2 - 200,
-        .width = 400, .height = 400
-    };
+    Rectangle boundry = {.x = MONITOR_WIDTH,
+        .y = MONITOR_HEIGHT,
+        .width = 800,
+        .height = 800};
 
     printf("Width: %d Height: %d \n", MONITOR_WIDTH, MONITOR_HEIGHT);
 
-    square.pos = (Vector2){
-        .x = MONITOR_WIDTH/2 - square.size.x/2, 
-        .y = MONITOR_HEIGHT/2 - square.size.x/2
-    };
+    square.pos = (Vector2){.x = MONITOR_WIDTH / 2 - square.size.x / 2,
+        .y = MONITOR_HEIGHT / 2 - square.size.x / 2};
 
     Camera2D camera = {0};
     camera.zoom = 1.0f;
 
+    Vector2 prev_window = GetWindowPosition();
+
     SetTargetFPS(60);
-	while (!WindowShouldClose()) {
+    while (!WindowShouldClose()) {
         float dt = GetFrameTime();
 
-        Vector2 window_pos = GetWindowPosition();
-        camera.target = window_pos;
+        Vector2 current_window = GetWindowPosition();
+        camera.target = current_window;
 
-        boundry.x = window_pos.x  + boundry.width/2;
-        boundry.y = window_pos.y + boundry.height/2;
+        boundry.x = current_window.x;
+        boundry.y = current_window.y;
 
-        update_square(&square, &boundry, dt);
+        update_square(&square, &boundry, prev_window, current_window, dt);
 
-		BeginDrawing();
+        prev_window = current_window;
+
+
+        BeginDrawing();
             BeginMode2D(camera);
                 ClearBackground(BLACK);
                 DrawRectangleV(square.pos, square.size, square.color);
                 DrawRectangleLines(boundry.x, boundry.y, boundry.width, boundry.height, RED);
             EndMode2D();
-		EndDrawing();
-	}
-	CloseWindow();
-	return 0;
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
 }
